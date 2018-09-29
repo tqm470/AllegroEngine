4928672162d3450eb7e8562a06705d7c7a06ad2c
Thiago Quadros Medeiros <tqm470@gmail.com>
2018-09-28 21:03:09 -0300

    Rotação adicionada e const correction melhorada

---
+++ Headers/Projetil.h
@@ -0,0 +1,11 @@
+#ifndef PROJETIL_CLASS
+#define PROJETIL_CLASS
+
+class Projetil
+{
+public:
+    Projetil::Projetil( const Imagem& sprite, const Vec2<float>& pos, const Vec2<float>& dir );
+
+};
+
+#endif
